0acf71b910ad73d466c4f56aeacaab949b709691
Todd Germaine <trgermaine@gmail.com>
2016-07-01 16:50:52 -0400

    added week 10 files from Tuesday

--- Week10/10.2/bank.js
+++ Week10/10.2/bank.js
@@ -3,38 +3,40 @@ var fs = require('fs');
 var transaction = process.argv[2];
 //total withdraw, deposit, lotto
 
-var amount = process.argv[3];
+var amount = parseFloat(process.argv[3]);
 //amount
 
-if (transaction === 'deposit') {
-	console.log('deposit! ' + amount);
+// Results
 
+
+
+if (transaction === 'deposit') {
+	fs.appendFile('bank_txt.txt', amount + ",");
 
 } else if (transaction === "total") {
-	
+		
+		var data = fs.readFile('bank_txt.txt', 'utf8', function(err, data) {
+
+		var split = data.split(',')
+
+		var results = 0;
+
+		console.log(split);
+		// For Loop
+
+		for (var i = 0; i < split.length; i++) {
+		 		results += parseFloat(split[i]);
+		 }
+		});
+
+
+		var total = results + amount;
 
 } else if (transaction === "withdraw") {
-	console.log('withdraw ' + amount);
+	fs.appendFile('bank_txt.txt', '-' + amount + ",");
 
 } else if (transaction === "lotto") {
 	//lotto
 }
 
-fs.readFile('bank_txt.txt', 'utf8', function(err, data) {
 
- var split = data.split(',')
-
- var results = 0;
-
- for (var i = 0; i < split.length; i++) {
- 		results += parseFloat(split[i]);
-
- 	// fs.appendFile('bank_txt.txt', 'utf8', function(split) {
-
- 	// });
-
- 	
-
- }
-console.log('total is ' + results);
-});
