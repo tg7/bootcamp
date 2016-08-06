82369f750169aa7a1af1bf0f1854e6bf36d39a7a
Todd Germaine <trgermaine@gmail.com>
2016-08-06 12:00:17 -0400

    updated saturday y files

--- Week14/14.3/1-QuotesApp/server.js
+++ Week14/14.3/1-QuotesApp/server.js
@@ -84,3 +84,4 @@ var port = 3000;
 app.listen(port, function () {
 	console.log('Listening on PORT ' + port);
 });
+
--- Week14/14.3/db/server.js
+++ Week14/14.3/db/server.js
@@ -1,3 +1,20 @@
-var orm = require ('./orm.js')
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'parties_db'
+});
 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  };
+
+  console.log('connected as id ' + connection.threadId);
+
+});
+
+var orm = require ('./orm.js')
 orm.selectAndOrder('*', 'parties', 'party_cost', 'DESC');
