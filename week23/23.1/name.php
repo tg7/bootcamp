ebe470d2090467c53aa4eb853c40f85499822766
Todd Germaine <trgermaine@gmail.com>
2016-09-20 19:19:08 -0400

    added week 23 folders

---
+++ week23/23.1/name.php
@@ -0,0 +1,20 @@
+<!-- Create a new PHP file. Name it whatever you wish.
+
+<?php
+
+$firstName = "Todd";
+
+$lastName = "Germaine";
+
+$birthYear = 1989;
+
+$fullName = "$firstName" . "$lastName";
+
+$age = 2016 - $birthYear;
+
+echo "My name is $fullName\n";
+
+echo "Im $age years old.\n";
+
+?>
+
